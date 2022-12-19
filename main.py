b96e2a2caf8c454ac65db14bda6ad5f06a281d01
achoolucgust <76622722+achoolucgust@users.noreply.github.com>
2022-12-20 01:12:16 +0800

    restart function

--- main.py
+++ main.py
@@ -2,61 +2,63 @@ from all_of_the_blocks_list_dont_touch import all as blockslist
 from PIL import Image, ImageStat
 from math import sqrt
 import sys
+def rest():
+    def image_grid(imgs, rows, cols):
+        if len(imgs) == rows*cols:
+            raise("r,c not correct")
 
-def image_grid(imgs, rows, cols):
-    if len(imgs) == rows*cols:
-        raise("r,c not correct")
+        w, h = imgs[0].size
+        grid = Image.new('RGB', size=(cols*w, rows*h))
+        grid_w, grid_h = grid.size
+        i = 0
+        for img in imgs:
+            sys.stdout.write(f"\r{i%cols*w}-{i//cols*h}        \t        \t")
+            grid.paste(img, box=(i%cols*w, i//cols*h))
+            i+= 1
+        return grid
 
-    w, h = imgs[0].size
-    grid = Image.new('RGB', size=(cols*w, rows*h))
-    grid_w, grid_h = grid.size
-    i = 0
-    for img in imgs:
-        sys.stdout.write(f"\r{i%cols*w}-{i//cols*h}        \t        \t")
-        grid.paste(img, box=(i%cols*w, i//cols*h))
-        i+= 1
-    return grid
+    def closest_color(rgb):
+        r, g, b, _ = rgb
+        color_diffs = []
+        texture = ""
+        for color in blockslist:
+            if len(color[0]) > 3:
+                cr, cg, cb, _ = tuple(color[0])
+            else:
+                cr, cg, cb = tuple(color[0])
+            color_diff = sqrt((r - cr)**2 + (g - cg)**2 + (b - cb)**2)
+            color_diffs.append((color_diff, color))
+        return min(color_diffs)[1]
 
-def closest_color(rgb):
-    r, g, b, _ = rgb
-    color_diffs = []
-    texture = ""
-    for color in blockslist:
-        if len(color[0]) > 3:
-            cr, cg, cb, _ = tuple(color[0])
-        else:
-            cr, cg, cb = tuple(color[0])
-        color_diff = sqrt((r - cr)**2 + (g - cg)**2 + (b - cb)**2)
-        color_diffs.append((color_diff, color))
-    return min(color_diffs)[1]
+    img_name = input("Enter filename with file extension [png, jpeg, jpg] like 'discord.png':\n")
 
-img_name = input("Enter filename with file extension [png, jpeg, jpg] like 'discord.png':\n")
+    src_img = Image.open(img_name)
+    w,h = src_img.size
 
-src_img = Image.open(img_name)
-w,h = src_img.size
+    texture_cache = {}
 
-texture_cache = {}
+    textures = []
+    #blockstoskip = input("How many blocks should it skip? (amount of pixels to skip to reduce file size and difficulty to build, e.x. `2` would skip 2 pixels)\n")
+    print("Finding correct textures for colors...\n\n")
+    for y in range(0,h):
+        for x in range(0,w):
+            texture = closest_color(src_img.getpixel((x,y)))[1]
+            texloaded = None
+            keyabletext = texture.replace("_","").replace(".","")
+            try:
+                texture_cache[keyabletext]
+            except:
+                sys.stdout.write(f"\rCache of {keyabletext} for {x}-{y} doesn't exist! Creating...")
+                texture_cache[keyabletext] = Image.open("blocks/s/" + texture)
+                texloaded = texture_cache[keyabletext]
+            else:
+                sys.stdout.write(f"\rCache of {keyabletext} for {x}-{y} exists!        \t        \t")
+                texloaded = texture_cache[keyabletext]
+            textures.append(texloaded)
+    print("\n\nStitching textures together...\n")
 
-textures = []
-#blockstoskip = input("How many blocks should it skip? (amount of pixels to skip to reduce file size and difficulty to build, e.x. `2` would skip 2 pixels)\n")
-print("Finding correct textures for colors...\n\n")
-for y in range(0,h):
-    for x in range(0,w):
-        texture = closest_color(src_img.getpixel((x,y)))[1]
-        texloaded = None
-        keyabletext = texture.replace("_","").replace(".","")
-        try:
-            texture_cache[keyabletext]
-        except:
-            sys.stdout.write(f"\rCache of {keyabletext} for {x}-{y} doesn't exist! Creating...")
-            texture_cache[keyabletext] = Image.open("blocks/s/" + texture)
-            texloaded = texture_cache[keyabletext]
-        else:
-            sys.stdout.write(f"\rCache of {keyabletext} for {x}-{y} exists!        \t        \t")
-            texloaded = texture_cache[keyabletext]
-        textures.append(texloaded)
-print("\n\nStitching textures together...\n")
+    image_grid(textures,h+1,w).save("final.png")
 
-image_grid(textures,h+1,w).save("final.png")
-
-input("\n\nFinished, [ENTER] to close\n")
+    input("\n\nFinished, [ENTER] to restart, [CTRL] + [C] to close\n")
+    rest()
+rest()
